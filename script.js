5834f6998d8ec6f8fa6e4c971241729ff1c0d29c
Dorian-F1 <94169807+Dorian-F1@users.noreply.github.com>
2024-04-11 11:03:23 +0200

    test aff2

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ async function afficherResultatsVote() {
 
         // Afficher les sénateurs qui ont voté "Pour"
         if (resultatsVote['Pour']) {
-            divPour.innerHTML = 'Ont voté pour :' + resultatsVote['Pour'].map(senateur => `<p>${senateur.name}</p>`).join('');
+            divPour.innerHTML = 'Ont voté pour :' + resultatsVote['Ont voté pour'].map(senateur => `<p>${senateur.name}</p>`).join('');
         }
 
         // Afficher les sénateurs qui ont voté "Contre"
